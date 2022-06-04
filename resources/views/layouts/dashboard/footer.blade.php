eb32c74a226bd5ccb52ebb90ae14c6a7f5f253e7
irmaulidia <irmamaulidiaaa@gmail.com>
2022-06-04 13:46:39 +0700

    SPP

---
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="https://www.creative-tim.com">
+                  SPP
+                
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com" target="_blank">E-SPP</a> for a better web.
+          </div>
+          <!-- your footer here -->
+        </div>
+      </footer>
